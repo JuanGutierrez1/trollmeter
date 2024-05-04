fb1328264dc528697f7eac687f5a643b123f8bce
Juan Gutierrez <jpgutierrez@globaltask.net>
2024-05-04 14:51:28 -0300

    Select added to the search input. Added some styles to display de percentage

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -8,10 +8,34 @@ interface InputProps {
 const Input = ({ onClick }: InputProps) => {
   const [value, setValue] = useState<string>('')
 
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onClick(value)
+    }
+  }
+
   return (
     <div className={styles['input-container']}>
-      <input onChange={(e) => setValue(e.target.value)} value={value} className={styles['input']} spellCheck={false} placeholder="Name#TAG" />
+      <input onKeyDown={onKeyDown} onChange={(e) => setValue(e.target.value)} value={value} className={styles['input']} spellCheck={false} placeholder="Name#TAG" />
       <i onClick={() => onClick(value)} className={`fas fa-search fa-2x ${styles['icon']}`}></i>
+      <select className={styles['select']} >
+        <option>NA</option>
+        <option>EUW</option>
+        <option>EUNE</option>
+        <option>OCE</option>
+        <option>KR</option>
+        <option>JP</option>
+        <option>BR</option>
+        <option>LAS</option>
+        <option>LAN</option>
+        <option>RU</option>
+        <option>TR</option>
+        <option>SG</option>
+        <option>PH</option>
+        <option>TW</option>
+        <option>VN</option>
+        <option>TH</option>
+      </select>
     </div>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,20 +2,42 @@ import { useState } from "react";
 import Input from "../../components/input/input";
 import styles from "./home.module.scss";
 
+const phrases = {
+  safe: ['Nice and safe!', 'We shilling 😎', 'Keep playing, is safe bro'],
+  danger: ['You are in danger!', 'Be careful!', 'Maybe you are unlucky'],
+  hell: ['You are in hell!', 'Consider not playing anymore'],
+  stop: ['Just stop playing!', 'Just create a new account'],
+}
+
 const Home = () => {
   const [trollMeter, setTrollMeter] = useState<number>(0);
 
   const getTrollMeterPhrase = () => {
-    if (trollMeter < 20) {
-      return "You are safe!";
-    } else if (trollMeter < 40) {
-      return "You are in danger!";
-    } else if (trollMeter < 60) {
-      return "You are in hell!";
-    } else if (trollMeter < 80) {
-      return "You are in the abyss!";
+    //TODO Make a phew phrases for each trollMeter range
+    if (trollMeter < 30) {
+      return (
+        <p style={{ color: '#198038', fontWeight: '700' }}>
+          {phrases.safe[Math.floor(Math.random() * (phrases.safe.length - 1))]}
+        </p>
+      )
+    } else if (trollMeter < 50) {
+      return (
+        <p style={{ color: '#ff832b', fontWeight: '700' }}>
+          {phrases.danger[Math.floor(Math.random() * (phrases.danger.length - 1))]}
+        </p>
+      )
+    } else if (trollMeter < 70) {
+      return (
+        <p style={{ color: '#da1e28', fontWeight: '700' }}>
+          {phrases.hell[Math.floor(Math.random() * (phrases.hell.length - 1))]}
+        </p>
+      )
     } else {
-      return "You are in the darkest abyss!";
+      return (
+        <p style={{ color: 'purple', fontWeight: '700' }}>
+          {phrases.stop[Math.floor(Math.random() * (phrases.stop.length - 1))]}
+        </p>
+      )
     }
   }
 
@@ -27,11 +49,16 @@ const Home = () => {
 
   return (
     <div className={styles['container']}>
+      <header className={styles['header']}>Que onda pa</header>
       <h1 className={styles['title']}>TROLLMETER</h1>
       <div className={styles['search-container']}>
         <Input onClick={(e) => calculateTrollMeter(e)} />
       </div>
-      <p className={styles['troll-meter']}>You were {trollMeter}% trolled. {getTrollMeterPhrase()}</p>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        <p className={styles['troll-percentage']}>{trollMeter}%</p>
+        <p className={styles['troll-meter']}>You were {trollMeter}% trolled in the last 20 games.</p>
+        <p className={styles['troll-phrase']}>{getTrollMeterPhrase()}</p>
+      </div>
     </div>
   );
 }
